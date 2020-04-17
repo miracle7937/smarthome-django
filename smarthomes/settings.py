17cb69664b18a795c03004f3fb6e70d93b3883b1
chukwude <ebukamiracle35@gmail.com>
2020-04-17 09:59:13 +0100

    ggg

--- smarthomes/settings.py
+++ smarthomes/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -183,8 +182,6 @@ AWS_DEFAULT_ACL = None
 '''
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 LOGGING = {
